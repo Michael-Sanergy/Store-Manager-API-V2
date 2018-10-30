3058c88cadc8eabc6a1ab2de12a6226fef971d56
unknown <kamaumike2013@gmail.com>
2018-10-30 13:29:54 +0300

    [Feature #161579134] Add '/products' endpoint

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -4,6 +4,7 @@ from flask_restplus import Api
 # Local import
 from .views import namespace_1 as n1
 from .views import namespace_2 as n2
+from .views import namespace_3 as n3
 
 # create a blueprint
 app_version2 = Blueprint("v2", __name__, url_prefix="/api/v2")
@@ -12,3 +13,4 @@ api_version2 = Api(app_version2)
 # Add namespaces
 api_version2.add_namespace(n1, path='/auth/signup')
 api_version2.add_namespace(n2, path='/auth/login')
+api_version2.add_namespace(n3, path='/products')
